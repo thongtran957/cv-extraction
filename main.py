952c7574b8a255f69c9bc199c6fe4ea755828774
thongtran957 <baibuvietnam@gmail.com>
2019-11-02 16:23:22 +0700

    edit

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ import os
 import pandas as pd
 import spacy
 import pattern as pt
+import nltk
+from nltk.tag.stanford import StanfordNERTagger
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords 
+from face_recognize import face_recognize
 
 # if code not working
 # import os
 # java_path = "C:/Program Files/Java/jdk1.8.0_221/bin/java.exe"
 # os.environ['JAVAHOME'] = java_path
 
-import nltk
-from nltk.tag.stanford import StanfordNERTagger
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords 
-
-from face_recognize import face_recognize
-
 def main():
-    file_path = 'pdf/Elliot-Alderson-Resume-Software-Developer-1.pdf'
+    file_path = 'pdf/PHP Web Developer Resume.pdf'
     text = ''
     for page in pdf2text.extract_text_from_pdf(file_path):
         text += ' ' + page
@@ -27,16 +25,59 @@ def main():
     phone = extract_mobile_number(text)
     address = extract_address(text)
     person_name = extract_person_name(text)
-    avatar_path = face_recognize(file_path)
+    # avatar_path = face_recognize(file_path)
     skills = extract_skills(text)
-    education = extract_education(text)
+    # education = extract_education(text)
+    # experience = extract_experience(text)
+    # competencie = extract_competencie(text, experience)
     print(person_name)
     print(emails)
     print(phone)
     print(address)
-    print(avatar_path)
+    # print(avatar_path)
     print(skills)
-    print(education)
+    # print(education)
+    # print(experience)
+    # print(competencie)
+
+
+def extract_experience(text):
+    word_tokens = nltk.tokenize.word_tokenize(text)
+    wordnet_lemmatizer = WordNetLemmatizer()
+    stop_words = pt.STOPWORDS
+    filtered_sentence = [w for w in word_tokens if not w in stop_words and wordnet_lemmatizer.lemmatize(w) not in stop_words] 
+    sent = nltk.pos_tag(filtered_sentence)
+    cp = nltk.RegexpParser('P: {<NNP>+}')
+    cs = cp.parse(sent)
+    
+    # for i in cs.subtrees(filter=lambda x: x.label() == 'P'):
+    #     print(i)
+    
+    test = []
+    
+    for vp in list(cs.subtrees(filter=lambda x: x.label()=='P')):
+        test.append(" ".join([i[0] for i in vp.leaves() if len(vp.leaves()) >= 2]))
+
+    # Search the word 'experience' in the chunk and then print out the text after it
+    x = [x[x.lower().index('experience') + 10:] for i, x in enumerate(test) if x and 'experience' in x.lower()]
+    return x
+
+def extract_competencie(text, experience_list):
+    '''
+    Helper function to extract competencies from resume text
+    :param resume_text: Plain resume text
+    :return: dictionary of competencies
+    '''
+    experience_text = ' '.join(experience_list)
+    competency_dict = {}
+
+    for competency in pt.COMPETENCIES.keys():
+        for item in pt.COMPETENCIES[competency]:
+            if string_found(item, experience_text):
+                if competency not in competency_dict.keys():
+                    competency_dict[competency] = [item]
+                else:
+                    competency_dict[competency].append(item)
 
 def extract_person_name(text):
     person_name = ''
@@ -126,5 +167,9 @@ def extract_education(atext):
             education.append(key)
     return education
 
+def string_found(string1, string2):
+    if re.search(r"\b" + re.escape(string1) + r"\b", string2):
+        return True
+    return False
 
 main()
